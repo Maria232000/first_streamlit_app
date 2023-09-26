0598cd22e863994d65b09ed5443d32077809d125
Maria232000 <95271068+Maria232000@users.noreply.github.com>
2023-09-26 15:57:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
-import streamlit
-streamlit.header('Breakfast Menu')
-import streamlit
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('kale,Spinach,Carrot Smoothie')
-streamlit.text('kale,Spinach,Carrot Smoothie')
-streamlit.text('Hard Boiled Free Ranged Egg')
+
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 kale,Spinach,Carrot Smoothie')
+streamlit.text('🐔Hard Boiled Free Ranged Egg')
+streamlit.text('🥑🍞 Avacado Toast')
