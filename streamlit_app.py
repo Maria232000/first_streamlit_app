235534ee6bf3a8d8b2156c0ed4a0fd9dd5cfc496
Maria232000 <95271068+Maria232000@users.noreply.github.com>
2023-09-27 17:41:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,17 +35,31 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
  
 
 streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
-try:
+#streamlit.header("Fruityvice Fruit Advice!")
+#create a repeatable code block(called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+ #New Section to display fruityvice api response
+ streamlit.header("Fruityvice Fruit Advice!")
+ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-   streamlit.error("Please select a fruit to get information.")
+    streamlit.error("Please select a fruit to get information.")
   else:
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+   back_from_function=get_fruitvice_data(fruit_choice)
+   streamlit.dataframe(back_from_function)
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#    streamlit.error("Please select a fruit to get information.")
+#   else:
+#    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+#    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#    streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#   streamlit.error()
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
@@ -53,7 +67,7 @@ except URLError as e:
 
 # write your own comment - what does this do?
 
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 
 
